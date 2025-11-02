ed766c8d177faf642f29f2cd72efade9af298eff
이준용 <1dlwnsdyd1@gmail.com>
2025-11-02 23:07:36 +0900

    Fix: Utils 추가

--- src/main/java/jwp/controller/LoginController.java
+++ src/main/java/jwp/controller/LoginController.java
@@ -1,13 +1,7 @@
 package jwp.controller;
-
-//import core.db.MemoryUserRepository;
 import jwp.dao.UserDao;
 import jwp.model.User;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import jwp.support.session.UserSessionUtils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +10,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class LoginController implements Controller {
-    private static final String USER_SESSION_KEY = "user";
+//    private static final String USER_SESSION_KEY = "user";
 //    private static final MemoryUserRepository userRepository = MemoryUserRepository.getInstance();
     private final UserDao userDao = new UserDao();
     @Override
--- src/main/java/jwp/controller/LogoutController.java
+++ src/main/java/jwp/controller/LogoutController.java
@@ -1,5 +1,7 @@
 package jwp.controller;
 
+import jwp.support.session.UserSessionUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,13 +11,13 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutController implements Controller {
-    private static final String USER_SESSION_KEY = "user";
+//    private static final String USER_SESSION_KEY = "user";
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(false);
         if (session != null) {
-            session.removeAttribute(USER_SESSION_KEY);
+            session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
         }
         return "redirect:/";
     }
